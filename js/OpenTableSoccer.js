b1ec5fd698602fa51aa6579716e3a2e1f355dbf6
rokie95 <motiurga@gmail.com>
2019-09-22 21:18:41 -0700

    Able to slingshot players and adjusted player weights to be more realistic

--- js/OpenTableSoccer.js
+++ js/OpenTableSoccer.js
@@ -14,6 +14,9 @@ var render = Matter.Render.create({
         wireframe: false
     }
 });
+var dragging = false;
+var draggingBody;
+var ctx = render.context;
 
 engine.world.gravity.y = 0;
 
@@ -37,15 +40,42 @@ Matter.World.add(world, mouseConstraint);
 
 Matter.Events.on(mouseConstraint, 'startdrag', function (evt) {
     console.log(evt);
+    dragging = true;
+    draggingBody = evt.body;
+    console.log("started dragging. draggingBody :");
+    console.log(draggingBody);
+
+});
+
+Matter.Events.on(mouseConstraint, 'enddrag', function (evt) {
+    var mousediffx = mouse.absolute.x - draggingBody.position.x;
+    var mousediffy = mouse.absolute.y - draggingBody.position.y;
+    var forceDamp = 300;
+
+    var maxdiff = 50;
+
+    if(mousediffx > maxdiff) { mousediffx = maxdiff; }
+    if(mousediffx < -maxdiff) { mousediffx = -maxdiff; }
+    if(mousediffy > maxdiff) { mousediffy = maxdiff; }
+    if(mousediffy < -maxdiff) { mousediffy = -maxdiff; }
+
+    draggingBody.force.x = -mousediffx / forceDamp;
+    draggingBody.force.y = -mousediffy / forceDamp;
+
+    dragging = false;
+    draggingBody = undefined;
+    console.log("stopped dragging");
+
+
 });
 
 render.mouse = mouse;
 
 var ball = Matter.Bodies.circle(cWidth / 2, cHeight / 2, 15, {
-    density: 0.001,
+    density: 0.0005,
     friction: 0.01,
-    frictionAir: 0.00001,
-    restitution: 0.8,
+    frictionAir: 0.01,
+    restitution: 0.9,
     render: {
         fillStyle: '#F35e66',
         strokeStyle: 'black',
@@ -55,19 +85,20 @@ var ball = Matter.Bodies.circle(cWidth / 2, cHeight / 2, 15, {
 
 var walls = [
     Matter.Bodies.rectangle(360, 0, cWidth, 5, { isStatic: true }),
-    Matter.Bodies.rectangle(105, 0, 3 / 10 * cWidth, 60, { isStatic: true }),
-    Matter.Bodies.rectangle(105 + 7 / 10 * cWidth, 0, 4 / 10 * cWidth, 60, { isStatic: true }),
+    Matter.Bodies.rectangle(120, 0, 3 / 10 * cWidth, 60, { isStatic: true }),
+    Matter.Bodies.rectangle(120 + 7 / 10 * cWidth, 0, 4 / 10 * cWidth, 60, { isStatic: true }),
     Matter.Bodies.rectangle(360, cHeight + 50, cWidth, 110, { isStatic: true }),
-    Matter.Bodies.rectangle(105, cHeight, 3 / 10 * cWidth, 60, { isStatic: true }),
-    Matter.Bodies.rectangle(105 + 7 / 10 * cWidth, cHeight, 4 / 10 * cWidth, 60, { isStatic: true }),
+    Matter.Bodies.rectangle(120, cHeight, 3 / 10 * cWidth, 60, { isStatic: true }),
+    Matter.Bodies.rectangle(120 + 7 / 10 * cWidth, cHeight, 4 / 10 * cWidth, 60, { isStatic: true }),
     Matter.Bodies.rectangle(0, 640, 20, cHeight + 10, { isStatic: true }),
     Matter.Bodies.rectangle(cWidth, 640, 20, cHeight + 10, { isStatic: true })
 ];
 
 var homePlayers = [
-    Matter.Bodies.circle(1 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 2 / 3, 32),
-    Matter.Bodies.circle(2 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 2 / 3, 40),
-    Matter.Bodies.circle(3 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 2 / 3, 32)
+    Matter.Bodies.circle(1 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 2 / 3, 32, {restitution : 0.2}),
+    Matter.Bodies.circle(2 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 2 / 3, 32, {restitution : 0.2}),
+    Matter.Bodies.circle(3 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 2 / 3, 32, {restitution : 0.2}),
+    Matter.Bodies.circle(2 / 3 * cWidth - (1 / 3 * cWidth) / 2, cHeight * 29 / 30, 40, {restitution : 0.2})
 ];
 
 
@@ -82,3 +113,37 @@ Matter.World.add(engine.world, ball);
 
 Matter.Engine.run(engine);
 Matter.Render.run(render);
+
+Matter.Events.on(render, 'afterRender', function(evt) {
+    if(dragging) {
+        var mousediffx = mouse.absolute.x - draggingBody.position.x;
+        var mousediffy = mouse.absolute.y - draggingBody.position.y;
+
+        var maxdiff = 50;
+
+        if(mousediffx > maxdiff) { mousediffx = maxdiff; }
+        if(mousediffx < -maxdiff) { mousediffx = -maxdiff; }
+        if(mousediffy > maxdiff) { mousediffy = maxdiff; }
+        if(mousediffy < -maxdiff) { mousediffy = -maxdiff; }
+
+        console.log("diffx: " + mousediffx + " diffy: " + mousediffy);
+
+        ctx.beginPath();
+        ctx.moveTo(draggingBody.position.x, draggingBody.position.y);
+        ctx.lineTo(draggingBody.position.x - mousediffx*2, draggingBody.position.y - mousediffy*2);
+        ctx.stroke();
+
+        console.log("dragging");
+    }
+});
+
+draw();
+
+function draw() {
+
+    if(dragging) {
+        
+    }
+
+    requestAnimationFrame(draw);
+}
